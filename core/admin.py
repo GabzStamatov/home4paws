2c319d17c17f2612736d840b5aff440643e32426
Gabriela Stamatov <youremail@whatever.com>
2026-04-24 14:29:26 +0100

    registered models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Shelter, Pet, AdoptionApplication, Favourite
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Shelter)
+admin.site.register(Pet)
+admin.site.register(AdoptionApplication)
+admin.site.register(Favourite)
