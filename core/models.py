4a903c2dab0aaf648c406ef3ece780ed370a4e86
Gabriela Stamatov <youremail@whatever.com>
2026-04-24 13:40:24 +0100

    added Profile model and migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
+#connects to Django User
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model): #stores role 
+    ROLE_CHOICES = [
+        ('ADOPTER', 'Adopter'),
+        ('STAFF', 'Shelter Staff'),
+        ('ADMIN', 'Admin'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
